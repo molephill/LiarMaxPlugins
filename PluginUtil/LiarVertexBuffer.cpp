b84262a649fcdb2afe4a22881b86aa77088d5510
liar <330446104@qq.com>
2018-07-13 17:56:45 +0800

    Read

--- PluginUtil/LiarVertexBuffer.cpp
+++ PluginUtil/LiarVertexBuffer.cpp
@@ -19,13 +19,4 @@ namespace Liar
 		delete uv;
 	}
 
-#ifdef PLUGINS
-	void LiarVertexBuffer::ParseMesh(Mesh* mesh, int index, bool zy)
-	{
-		//位置，要注意的是在3ds max中z值是朝上的，y值是朝前的，而在我们的游戏中,y值朝上，z值朝前。所以要做下处理。
-		position->ParseMaxPoint3(mesh->verts[index], zy);
-		normal->ParseMaxPoint3(mesh->getNormal(index), zy);
-	}
-#endif // PLUGINS
-
 }
